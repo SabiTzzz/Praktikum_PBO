b70590ba3b3725acfd030918da0c5546c5de874d
Sabitz <143915759+SabiTzzz@users.noreply.github.com>
2025-04-08 23:07:44 +0800

    Update Rutinan.java

--- posttest3/mydompet/src/main/java/com/mydompet/model/Rutinan.java
+++ posttest3/mydompet/src/main/java/com/mydompet/model/Rutinan.java
@@ -3,8 +3,7 @@ package com.mydompet.model;
 import java.time.LocalDate;
 
 public class Rutinan extends Transaksi {
-
-    public Rutinan(int id, String jenis, double jumlah, LocalDate tanggal, String keterangan, String kategori, String frekuensi) {
+    public Rutinan(int id, double jumlah, LocalDate tanggal, String keterangan, String kategori) {
         super(id, "Rutinan", jumlah, tanggal, keterangan, kategori);
     }
 
